5c877ccf763f88695cda32fd926ce366e20a43aa
DenisBindyukov <gitgroom@gmail.com>
2021-12-16 18:09:52 +0300

    new commit

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const path = require('path');
-//Движок для динамического составления файлов.
-const exphbs = require('express-handlebars');
-const homeRoutes = require('./routes/home');
-const addRoutes = require('./routes/add');
-const coursesRoutes = require('./routes/courses');
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
-const app = express();
+const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
-
-app.use(express.static('public'));
-//Прозмежуточный слой для пост запроса ( Заберает данные из буфера(UI) )
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+
+const password = 'KwYR9sXB2fgKXs00'
+const url = `mongodb+srv://den_flex:${password}@cluster0.6rqxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
--- routes/add.js
+++ routes/add.js
@@ -3,18 +3,18 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('add', {
-        title: 'Add course',
-        isAdd: true
-    })
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  })
 })
 
 router.post('/', async (req, res) => {
-const course = new Course(req.body.title, req.body.price, req.body.img);
+  const course = new Course(req.body.title, req.body.price, req.body.img)
 
-    await course.save();
+  await course.save()
 
-    res.redirect('/courses')
+  res.redirect('/courses')
 })
 
 module.exports = router
